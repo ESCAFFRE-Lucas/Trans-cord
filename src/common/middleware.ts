219000be5b2a87a03d096ae1d47b447ccb4f25d6
lucas <lucas.escaffre@ynov.com>
2023-11-16 11:51:47 +0100

    init: swaggerUi + openapi-validator

---
+++ src/common/middleware.ts
@@ -0,0 +1,14 @@
+import { Request, Response, NextFunction } from "express";
+import HttpException from "../exceptions/httpException";
+
+export const errorMiddleware = (error: HttpException, request: Request, response: Response, next: NextFunction) => {
+    const status = error.status || 500;
+    const message = error.message || 'Something went wrong';
+    if (process.env.NODE_ENV === "test") {
+        console.log(error);
+    }
+    response ? response.status(status).send({
+        status,
+        message,
+    }) : next();
+};

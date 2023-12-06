cf23d7d25999ba14f6f6286e811856e19a46d573
lucas <lucas.escaffre@ynov.com>
2023-12-06 14:15:30 +0100

    continue: test with jest

--- src/auth/auth.test.ts
+++ src/auth/auth.test.ts
@@ -1,8 +1,8 @@
-import prisma from "../lib/db";
 import { describe, expect } from '@jest/globals';
 import request from 'supertest';
 import { app } from '../index';
 import * as authService from './auth.service';
+import { prismaMock } from '../singleton'
 
 const userMock = {
     id: "44c571e3-69db-4ac5-915b-c613f7b1177d",
@@ -14,9 +14,9 @@ const userMock = {
     createdAt: new Date()
 }
 
-jest.mock('./auth.service', () => ({
+const mockAuthService = {
     getUserById: jest.fn(),
-}));
+};
 
 describe('test on controller', () => {
     beforeEach(() => {
@@ -42,25 +42,25 @@ describe('test on controller', () => {
         expect(response.body).toHaveProperty('token');
     });
 
-    it('should not register an user if already exists', async () => {
-        jest.spyOn(authService, 'getUserById').mockImplementation(async (id: string) => {
+    it('should not register a user if already exists', async () => {
+        mockAuthService.getUserById.mockImplementation(async (id) => {
             if (id === userMock.id) {
-                return userMock;
+                return { userMock };
             } else {
                 return null;
             }
         });
 
         const response = await request(app).post('/auth/register').send({
-            username: "test",
-            password: "password",
+            username: userMock.username,
+            password: userMock.password,
         });
 
         expect(response.status).toBe(400);
-        expect(response.body).not.toBeNull();
-        //expect(response.body).toHaveProperty('message', 'User already exists');
-    });
+        expect(response.body).toHaveProperty('message', 'User already exists');
 
+        expect(mockAuthService.getUserById).toHaveBeenCalledWith('existingUserId');
+    });
     it('should login an user', async () => {
 
         const response = await request(app).post('/auth/login').send({
@@ -71,5 +71,31 @@ describe('test on controller', () => {
         expect(response.status).toBe(200);
         expect(response.body).not.toBeNull();
         expect(response.body).toHaveProperty('token');
+    });
+});
+
+describe('test on service', () => {
+    beforeEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should add a new user', async () => {
+        prismaMock.user.create.mockResolvedValue(userMock);
+
+        await expect(authService.addUser({
+            username: "test",
+            password: "password",
+            discordId: "123456789",
+            role: "admin"
+        })).resolves.toEqual(userMock);
+        expect(prismaMock.user.create).toBeCalledTimes(1);
+        expect(prismaMock.user.create).toBeCalledWith({
+            data: {
+                username: "test",
+                password: "password",
+                discordId: "123456789",
+                role: "admin"
+            }
+        });
     });
 });

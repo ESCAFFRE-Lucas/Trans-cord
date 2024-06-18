5ad375cefddaaed4a811102904517039e2936b03
tonycava <anthony.cavge@gmail.com>
2024-06-18 09:30:02 +0200

    add: health and welcome route

--- server/index.ts
+++ server/index.ts
@@ -10,11 +10,22 @@ import cors from 'cors'
 import 'dotenv/config';
 import discordRouter from "./discord/discord.router";
 import userRouter from "./user/user.router";
+import { Client, IntentsBitField } from "discord.js";
+import prisma from "./lib/prisma";
 
 const port = +(process.env.PORT || 3000);
 export const app = express();
 
 app.use(cors());
+
+const client = new Client({
+	intents: [
+		IntentsBitField.Flags.Guilds,
+		IntentsBitField.Flags.GuildMembers,
+		IntentsBitField.Flags.GuildMessages,
+		IntentsBitField.Flags.MessageContent,
+	],
+});
 
 const swaggerOptions: SwaggerOptions = {
 	definition: {
@@ -41,6 +52,28 @@ app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 // app.use(OpenApiValidator.middleware({
 //  apiSpec: `${__dirname}/swagger-config.yml`,
 // }));
+
+app.get("/health", (req, res) => {
+	return res.json({ healthy: true })
+});
+
+app.get("/welcome", async (req, res) => {
+	const serverCount = client.guilds.cache.size;
+	const translationCount = await prisma.translate.count();
+	const languageCount = 25;
+
+	return res.json({
+		message: "Success",
+		status: 200,
+		data: {
+			serverCount,
+			translationCount,
+			languageCount,
+		}
+	})
+
+
+})
 
 app.use('/translate', translateRouter);
 app.use('/user', userRouter);
